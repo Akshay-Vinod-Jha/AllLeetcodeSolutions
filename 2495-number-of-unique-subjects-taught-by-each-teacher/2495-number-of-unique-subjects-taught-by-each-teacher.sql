8bffece6f6222e9e77de354d829dc5ccb8e80cb3
Jha Akshay Vinod <141563755+Akshay-Vinod-Jha@users.noreply.github.com>
2025-08-04 18:11:20 +0530

    Time: 671 ms (11.58%), Space: 0B (100%) - LeetHub

--- 2495-number-of-unique-subjects-taught-by-each-teacher/2495-number-of-unique-subjects-taught-by-each-teacher.sql
+++ 2495-number-of-unique-subjects-taught-by-each-teacher/2495-number-of-unique-subjects-taught-by-each-teacher.sql
@@ -4,8 +4,5 @@ WITH TABLE1 AS (
         DENSE_RANK() OVER(PARTITION BY teacher_id ORDER BY subject_id ) AS DENSERANK
         FROM
         Teacher 
-),
-TABLE2 AS (
-    SELECT teacher_id,MAX(DENSERANK ) AS cnt FROM TABLE1 GROUP BY teacher_id 
 )
-SELECT * FROM TABLE2
+SELECT teacher_id,MAX(DENSERANK ) AS cnt FROM TABLE1 GROUP BY teacher_id 
