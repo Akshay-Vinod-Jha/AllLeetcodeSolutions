95bfe99cd74e34bf30f7219e835427c7710453ec
Jha Akshay Vinod <141563755+Akshay-Vinod-Jha@users.noreply.github.com>
2026-06-16 22:40:45 +0530

    Time: 0 ms (100%), Space: 11.7 MB (78.55%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -4,34 +4,34 @@ public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
-        vector<vector<int>> dp(m,vector<int>(n,-1));
+        vector<int> dp(n,-1);
         if(obstacleGrid[0][0]==1 || obstacleGrid[m-1][n-1]) return 0;
         
-        dp[0][0]=1;
+        dp[0]=1;
         for(int xs=0;xs<m;xs++){
           for(int ys=0;ys<n;ys++){
             if(xs==0 && ys==0) continue;
             int up = 0;
             int uxs = xs-1;
             int uys = ys;
-            if(uxs>=0 && uys>=0 && dp[uxs][uys]!=-1 && obstacleGrid[uxs][uys]!=1){
-              up = dp[uxs][uys];
+            if(uxs>=0 && uys>=0 && dp[uys]!=-1 && obstacleGrid[uxs][uys]!=1){
+              up = dp[uys];
             }
 
             int right = 0;
             uxs = xs;
             uys = ys - 1;
-            if(uxs>=0 && uys>=0 && dp[uxs][uys]!=-1 && obstacleGrid[uxs][uys]!=1){
-              right = dp[uxs][uys];
+            if(uxs>=0 && uys>=0 && dp[uys]!=-1 && obstacleGrid[uxs][uys]!=1){
+              right = dp[uys];
             }
         
 
-            dp[xs][ys] = up + right;
+            dp[ys] = up + right;
 
           }
         }
 
-        return dp[m-1][n-1];
+        return dp[n-1];
 
     }
 };
