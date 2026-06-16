bb23ccaab0e37d9f12aa1f6a42bcd665e047347b
Jha Akshay Vinod <141563755+Akshay-Vinod-Jha@users.noreply.github.com>
2026-06-16 23:36:13 +0530

    Time: 2 ms (34.46%), Space: 16.4 MB (15.52%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,27 +1,47 @@
 class Solution {
 public:
-    int helper(int xs,int ys,vector<vector<int>>& grid,vector<vector<int>> &dp){
-        if(xs==0 && ys==0) return grid[xs][ys];
-        if(xs<0 || ys<0) return INT_MAX;
+    // int helper(int xs,int ys,vector<vector<int>>& grid,vector<vector<int>> &dp){
+    //     if(xs==0 && ys==0) return grid[xs][ys];
+    //     if(xs<0 || ys<0) return INT_MAX;
 
-        if(dp[xs][ys]!=-1) return dp[xs][ys];
+    //     if(dp[xs][ys]!=-1) return dp[xs][ys];
 
-        int up =  helper(xs-1,ys,grid,dp);
-        int down = helper(xs,ys-1,grid,dp);
+    //     int up =  helper(xs-1,ys,grid,dp);
+    //     int down = helper(xs,ys-1,grid,dp);
 
-        up = (up==INT_MAX)?INT_MAX:grid[xs][ys]+up;
-        down = (down==INT_MAX)?INT_MAX:grid[xs][ys]+down;
+    //     up = (up==INT_MAX)?INT_MAX:grid[xs][ys]+up;
+    //     down = (down==INT_MAX)?INT_MAX:grid[xs][ys]+down;
 
 
-        return  dp[xs][ys]=min(up,down);
+    //     return  dp[xs][ys]=min(up,down);
 
-    }
+    // }
     int minPathSum(vector<vector<int>>& grid) {
         
         int m = grid.size();
         int n = grid[0].size();
         vector<vector<int>> dp(m,vector<int>(n,-1));
-        return helper(m-1,n-1,grid,dp);
+        dp[0][0] = grid[0][0];
+        for(int xs=0;xs<m;xs++){
+            for(int ys=0;ys<n;ys++){
+                if(xs==0 &&  ys==0) continue;
+                int up = INT_MAX;
+                int uxs = xs-1;
+                int uys = ys;
+                if(uxs>=0 && uys>=0 && dp[uxs][uys]!=-1){
+                    up = grid[xs][ys] + dp[uxs][uys];
+                }
+
+                uxs = xs;
+                uys = ys-1;
+                int down = INT_MAX;
+                if(uxs>=0 && uys>=0 && dp[uxs][uys]!=-1){
+                    down = grid[xs][ys] + dp[uxs][uys];
+                }
+                dp[xs][ys] = min(up,down);
+            }
+        }
+        return dp[m-1][n-1];
 
     }
 };
