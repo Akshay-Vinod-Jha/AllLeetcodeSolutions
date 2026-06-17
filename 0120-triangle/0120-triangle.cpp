5cd489e7a0b5ea0153863da3a34505f34cdccf81
Jha Akshay Vinod <141563755+Akshay-Vinod-Jha@users.noreply.github.com>
2026-06-17 12:39:26 +0530

    Time: 6 ms (3.3%), Space: 12.9 MB (62.52%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -3,28 +3,31 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int rows = triangle.size();
         int noofcolumns = rows;
-        vector<vector<int>> dp(rows,vector<int>(noofcolumns,-1));
-        dp[0][0] = triangle[0][0];
+        vector<int> dp(noofcolumns,-1);
+        dp[0] = triangle[0][0];
         for(int currentrow=1;currentrow<rows;currentrow++){
+            vector<int> temp(noofcolumns,-1);
             for(int coltaken=0;coltaken<=currentrow;coltaken++){
+
                 int ekpiche = coltaken - 1;
                 int upar = coltaken;
                 int ekpicheres = INT_MAX;
                 if(ekpiche>=0 && ekpiche<currentrow){
-                    ekpicheres = min(ekpicheres,triangle[currentrow][coltaken]+dp[currentrow-1][ekpiche]);
+                    ekpicheres = min(ekpicheres,triangle[currentrow][coltaken]+dp[ekpiche]);
                 }
                 int uparres = INT_MAX;
                 if(upar>=0 && upar<currentrow){
-                    uparres = min(uparres,triangle[currentrow][coltaken]+dp[currentrow-1][upar]);
+                    uparres = min(uparres,triangle[currentrow][coltaken]+dp[upar]);
                 }
 
-                dp[currentrow][coltaken] = min(ekpicheres,uparres);
+                temp[coltaken] = min(ekpicheres,uparres);
             }
+            dp = temp;
         }
 
         int minis = INT_MAX;
         for(int i=0;i<noofcolumns;i++){
-            minis = min(minis,dp[rows-1][i]);
+            minis = min(minis,dp[i]);
         }
         return minis;
     }
