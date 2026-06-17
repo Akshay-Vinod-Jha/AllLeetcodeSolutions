35e4723def5c0e328426afb76dd232914e6fade6
Jha Akshay Vinod <141563755+Akshay-Vinod-Jha@users.noreply.github.com>
2026-06-17 12:34:53 +0530

    Time: 4 ms (6.44%), Space: 13.3 MB (55.01%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,45 +1,30 @@
 class Solution {
 public:
-    int helper(int currentrow,int crnoofcol,int coltaken,vector<vector<int>>& triangle,vector<vector<int>>& dp){
-        
-        if(currentrow<0 || crnoofcol<0) return 0;
-
-        if(dp[currentrow][coltaken]!=-1) return dp[currentrow][coltaken];
-
-        int ekpiche = coltaken - 1;
-        int upar = coltaken;
-
-
-        int ekpicheres = INT_MAX;
-        if(ekpiche>=0 && ekpiche<crnoofcol){
-            int consme = helper(currentrow-1,crnoofcol-1,ekpiche,triangle,dp);
-            if(consme!=INT_MAX){
-                ekpicheres = min(ekpicheres,triangle[currentrow][ekpiche] + consme);
-            }
-        }   
-
-        int uparres = INT_MAX;
-        if(upar>=0 && upar<crnoofcol){
-            int consme = helper(currentrow-1,crnoofcol-1,upar,triangle,dp);
-            if(consme!=INT_MAX){
-                uparres = min(uparres,triangle[currentrow][upar] + consme);
-            }
-        }
-
-        return dp[currentrow][coltaken]=min(ekpicheres,uparres);
-    }
     int minimumTotal(vector<vector<int>>& triangle) {
         int rows = triangle.size();
         int noofcolumns = rows;
-        int minis = INT_MAX;
         vector<vector<int>> dp(rows,vector<int>(noofcolumns,-1));
-        for(int i=0;i<noofcolumns;i++){
-            int consme =helper(rows-2,noofcolumns-1,i,triangle,dp);
-            if(consme!=INT_MAX){
-                
-                minis = min(minis, triangle[rows-1][i] + consme) ;
+        dp[0][0] = triangle[0][0];
+        for(int currentrow=1;currentrow<rows;currentrow++){
+            for(int coltaken=0;coltaken<=currentrow;coltaken++){
+                int ekpiche = coltaken - 1;
+                int upar = coltaken;
+                int ekpicheres = INT_MAX;
+                if(ekpiche>=0 && ekpiche<currentrow){
+                    ekpicheres = min(ekpicheres,triangle[currentrow][coltaken]+dp[currentrow-1][ekpiche]);
+                }
+                int uparres = INT_MAX;
+                if(upar>=0 && upar<currentrow){
+                    uparres = min(uparres,triangle[currentrow][coltaken]+dp[currentrow-1][upar]);
+                }
 
+                dp[currentrow][coltaken] = min(ekpicheres,uparres);
             }
+        }
+
+        int minis = INT_MAX;
+        for(int i=0;i<noofcolumns;i++){
+            minis = min(minis,dp[rows-1][i]);
         }
         return minis;
     }
