c835f9c0318d2e547673adee0b38f67ab1325427
Jha Akshay Vinod <141563755+Akshay-Vinod-Jha@users.noreply.github.com>
2026-06-17 20:11:20 +0530

    Time: 58 ms (34.39%), Space: 21.1 MB (26.85%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,36 +1,37 @@
 class Solution {
 public:
-    int helper(int i,int j1,int j2,vector<vector<int>>& grid,int rows,int cols,vector<vector<vector<int>>>& dp){
-        if(j1<0 || j1>=cols || j2<0 || j2>=cols) return INT_MIN;
-
-        if(dp[i][j1][j2]!=-1) return dp[i][j1][j2];
-
-        if(i==rows-1){
-            if(j1==j2) return grid[i][j1];
-            return grid[i][j1] + grid[i][j2];
-        }
-
-
-
-        int toadd = (j1==j2)?grid[i][j1]:grid[i][j1]+grid[i][j2];
-
-        int maxis = INT_MIN;
-        for(int rb1=-1;rb1<=1;rb1++){
-            for(int rb2=-1;rb2<=1;rb2++){
-                int ret = helper(i+1,j1+rb1,j2+rb2,grid,rows,cols,dp);
-                if(ret!=INT_MIN){
-                    maxis = max(maxis,toadd+ret);
-                }
-            }
-        }
-
-        return dp[i][j1][j2]=maxis;
-
-    }
     int cherryPickup(vector<vector<int>>& grid) {
         int rows = grid.size();
         int cols = grid[0].size();
         vector<vector<vector<int>>> dp(rows,vector<vector<int>>(cols,vector<int>(cols,-1)));
-        return helper(0,0,cols-1,grid,rows,cols,dp);
+        for(int j1=0;j1<cols;j1++){
+            for(int j2=0;j2<cols;j2++){
+                if(j1!=j2){
+                    dp[rows-1][j1][j2] = grid[rows-1][j1] + grid[rows-1][j2];
+                }else{
+                    dp[rows-1][j1][j2] = grid[rows-1][j1];
+                }
+                
+            }
+        }
+        for(int i = rows-2;i>=0;i--){
+        for(int j1=0;j1<cols;j1++){
+            for(int j2=0;j2<cols;j2++){
+                int toadd = (j1==j2)?grid[i][j1]:grid[i][j1]+grid[i][j2];
+                int maxis = INT_MIN;
+                for(int rb1=-1;rb1<=1;rb1++){
+                    for(int rb2=-1;rb2<=1;rb2++){
+                        int nextj1 = j1 + rb1;
+                        int nextj2 = j2 + rb2;
+                        if(nextj1>=0 && nextj1<cols && nextj2>=0 && nextj2<cols){
+                            maxis = max(maxis,toadd+dp[i+1][nextj1][nextj2]);
+                        }
+                    }
+                }
+                dp[i][j1][j2] = maxis;
+            }
+            }
+        }
+        return dp[0][0][cols-1];
     }
 };
