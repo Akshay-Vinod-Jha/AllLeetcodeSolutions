7f31adeb1ef450c8df5e758ae848ae2be2a997f5
Jha Akshay Vinod <141563755+Akshay-Vinod-Jha@users.noreply.github.com>
2026-06-18 20:48:35 +0530

    Time: 329 ms (5%), Space: 20.8 MB (51.51%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,37 +1,37 @@
 class Solution {
 public:
-    bool helper(
-        int index,
-        int sum,
-        int current,
-        vector<int>& nums,
-        vector<vector<int>>& dp
-    ){
-
-        if(index==-1){
-            int rem = sum - current;
-            return (rem==current);
-        }  
-
-        if(dp[index+1][current]!=-1) return dp[index+1][current];
-
-        int rem = sum - current;
-        if(rem==current) return true;
-
-        
- 
-        bool include = helper(index-1,sum,current+nums[index],nums,dp);
-        bool exclude = helper(index-1,sum,current,nums,dp);
-
-        return  dp[index+1][current] = include || exclude;
-
-    }
     bool canPartition(vector<int>& nums) {
 
         int size = nums.size();
+
         int sum = 0;
-        for(auto& i:nums) sum += i;
-        vector<vector<int>> dp(size+2,vector<int>(sum+1,-1));
-        return helper(nums.size()-1,sum,0,nums,dp);
+        for(auto &i : nums) sum += i;
+
+        if(sum % 2) return false;
+
+        vector<vector<bool>> dp(size + 1,
+                                vector<bool>(sum + 1,false));
+
+        dp[0][0] = true;
+
+        for(int index = 1; index <= size; index++){
+
+            for(int current = 0; current <= sum; current++){
+
+                bool exclude = dp[index-1][current];
+
+                bool include = false;
+
+                if(current >= nums[index-1]){
+                    include =
+                        dp[index-1][current - nums[index-1]];
+                }
+
+                dp[index][current] =
+                    include || exclude;
+            }
+        }
+
+        return dp[size][sum/2];
     }
 };
