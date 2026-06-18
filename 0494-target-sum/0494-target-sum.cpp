e48d8120898b06c226808b3f0d4e67755f4b24aa
Jha Akshay Vinod <141563755+Akshay-Vinod-Jha@users.noreply.github.com>
2026-06-18 19:56:28 +0530

    Time: 20 ms (43.33%), Space: 25.8 MB (24.72%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,21 +1,42 @@
 class Solution {
-public: 
-    int helper(int index,int target,vector<int>& nums){
-        if(index==-1){
-            return (target==0)?1:0;
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+
+        int size = nums.size();
+
+        int sum = 0;
+        for(auto &i : nums) sum += i;
+
+        if(target > sum || target < -sum) return 0;
+
+        vector<vector<int>> dp(size + 1,
+                               vector<int>(2 * sum + 1, 0));
+
+        dp[0][sum] = 1;
+
+        for(int index = 1; index <= size; index++) {
+
+            for(int currenttarget = -sum;
+                currenttarget <= sum;
+                currenttarget++) {
+
+                int plus = 0;
+                int minus = 0;
+
+                int prevtarget = currenttarget - nums[index - 1];
+                if(prevtarget >= -sum && prevtarget <= sum) {
+                    plus = dp[index - 1][prevtarget + sum];
+                }
+
+                prevtarget = currenttarget + nums[index - 1];
+                if(prevtarget >= -sum && prevtarget <= sum) {
+                    minus = dp[index - 1][prevtarget + sum];
+                }
+
+                dp[index][currenttarget + sum] = plus + minus;
+            }
         }
 
-        int plus = helper(index-1,target-nums[index],nums);
-        int minus = helper(index-1,target+nums[index],nums);
-
-        return plus + minus;
-
-    }
-    int findTargetSumWays(vector<int>& nums, int target) {
-        int size = nums.size();
-        return helper(size-1,target,nums);
+        return dp[size][target + sum];
     }
 };
-
-
-
