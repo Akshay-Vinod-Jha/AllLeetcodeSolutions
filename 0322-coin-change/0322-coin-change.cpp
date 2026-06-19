377d0cd99f8986af1bc7d97ddae2eaf03bdfda60
Jha Akshay Vinod <141563755+Akshay-Vinod-Jha@users.noreply.github.com>
2026-06-19 23:26:03 +0530

    Time: 51 ms (23.21%), Space: 49 MB (34%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,38 +1,31 @@
 class Solution {
 public:
-    int helper(
-        int index,
-        int target,
-        vector<int>& coins,
-        vector<vector<int>>& dp
-    ){
-        if(target==0) return 0;
-
-        if(index==0){
-            if(target%coins[0] ==  0){
-                return target / coins[0];
-            }
-            return INT_MAX;
-        }
-
-        if(dp[index][target]!=-1) return dp[index][target];
-
-        int cont = INT_MAX;
-        if(coins[index]<=target){
-            int returned = helper(index,target-coins[index],coins,dp);
-            if(returned!=INT_MAX){
-                cont = 1 + returned;
-            }
-        }
-
-        int skip = helper(index-1,target,coins,dp);
-        return  dp[index][target] = min(cont,skip);
-
-    }
     int coinChange(vector<int>& coins, int amount) {
         int size = coins.size();
-        vector<vector<int>> dp(size,vector<int>(amount+1,-1));
-        int retme = helper(size-1,amount,coins,dp);
-        return retme==INT_MAX?-1:retme;
+        vector<vector<int>> dp(size,vector<int>(amount+1,INT_MAX));
+        for(int i=0;i<size;i++){
+            dp[i][0] = 0;
+        }
+        for(int i=0;i<amount+1;i++){
+            dp[0][i] = (i%coins[0]==0)? (i/coins[0]) : INT_MAX;
+        }
+        
+        for(int index=1;index<size;index++){
+            for(int target=1;target<amount+1;target++){
+                int cont = INT_MAX;
+
+                if(coins[index] <= target){
+                    int ret = dp[index][target-coins[index]];
+                
+                    if(ret != INT_MAX){
+                        cont = 1 + ret;
+                    }
+                }
+                int skip = dp[index-1][target];
+                dp[index][target] = min(cont,skip);
+            }
+        }
+        
+        return dp[size-1][amount]==INT_MAX?-1:dp[size-1][amount];
     }
 };
