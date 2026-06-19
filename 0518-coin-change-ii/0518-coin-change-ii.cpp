828f6172acf342897e39d0ac688dd40bd21947b5
Jha Akshay Vinod <141563755+Akshay-Vinod-Jha@users.noreply.github.com>
2026-06-19 23:58:52 +0530

    Time: 31 ms (48.78%), Space: 60.6 MB (16.19%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -3,24 +3,27 @@ public:
     int change(int amount, vector<int>& coins) {
         int sum = amount;
         int size = coins.size();
-        vector<vector<long long>> dp(size,vector<long long>(sum+1,0));
+        vector<long long> dp(sum+1,0);
         for(int i=0;i<size;i++){
-            dp[i][0] = 1;
+            dp[0] = 1;
         }
         for(int i=0;i<sum+1;i++){
-            dp[0][i] = (i%coins[0]==0)? 1 : 0;
+            dp[i] = (i%coins[0]==0)? 1 : 0;
         }
         for(int index=1;index<size;index++){
+            vector<long long> temp(sum+1,0);
+            temp[0] = 1;
             for(int target=1;target<sum+1;target++){
                 int cont = 0;
 
                 if(coins[index] <= target){
-                    cont = dp[index][target-coins[index]];
+                    cont = temp[target-coins[index]];
                 }
-                int skip = dp[index-1][target];
-                dp[index][target] =(long long)cont+(long long)skip;
+                int skip = dp[target];
+                temp[target] =(long long)cont+(long long)skip;
             }
+            dp = temp;
         }
-        return dp[size-1][sum];
+        return dp[sum];
     }
 };
