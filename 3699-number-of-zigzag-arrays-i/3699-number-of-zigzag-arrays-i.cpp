bc1c1a9b7f036bc801767b025dbc207da89a1e48
Jha Akshay Vinod <141563755+Akshay-Vinod-Jha@users.noreply.github.com>
2026-06-24 21:05:49 +0530

    Time: 846 ms (43.24%), Space: 51.4 MB (41.44%) - LeetHub

--- 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.cpp
+++ 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.cpp
@@ -1,117 +1,51 @@
-// class Solution {
-// public:
-//     long long modme = 1e9 + 7;
-//     long long helper(int l,int r,int i,int s,int prev,vector<vector<vector<int>>>& dp){
-//         if(i==0) return 1;
-
-//         if(dp[i][prev][s]!=-1) return dp[i][prev][s];
-
-//         int szero = 0;
-//         if(s!=0){
-//            for(int hehe=prev+1;hehe<=r;hehe++){
-//             szero = (szero + helper(l,r,i-1,0,hehe,dp))%modme;
-//            }
-//         }
-//         int sone = 0;
-//         if(s!=1){
-//             for(int hehe=prev-1;hehe>=l;hehe--){
-//                sone = ( sone + helper(l,r,i-1,1,hehe,dp)) % modme;
-//             }
-//         }
-
-//         return dp[i][prev][s]=(sone+szero)%modme;
-
-//     }
-//     int zigZagArrays(int n, int l, int r) {
-//         long long retans = 0;
-//         vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(r+1,vector<int>(3,-1)));
-//         for(int i=l;i<=r;i++){
-//             retans = (retans + helper(l,r,n-1,2,i,dp))%modme;
-//         }
-//         return retans;
-//     }
-// };
-
-
-// class Solution {
-// public:
-//     long long modme = 1e9 + 7;
-//     int zigZagArrays(int n, int l, int r) {
-//         vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(r+1,vector<int>(3,1)));
-//         for(int i=1;i<n;i++){
-//             for(int prev=l;prev<=r;prev++){
-//                 for(int s=0;s<3;s++){
-//                     int szero = 0;
-//                     if(s!=0){
-//                     for(int hehe=prev+1;hehe<=r;hehe++){
-//                         szero = (szero + dp[i-1][hehe][0])%modme;
-//                     }
-//                     }
-//                     int sone = 0;
-//                     if(s!=1){
-//                         for(int hehe=prev-1;hehe>=l;hehe--){
-//                         sone = ( sone + dp[i-1][hehe][1]) % modme;
-//                         }
-//                     } 
-//                     dp[i][prev][s]=(sone+szero)%modme;
-//                 }
-//             }
-//         }
-//         int retans = 0;
-//         for(int i=l;i<=r;i++){
-//             retans =( retans + dp[n-1][i][2])%modme;
-//         }
-//         return retans;
-//     }
-// };
-
 class Solution {
 public:
-    static const int MOD = 1000000007;
+    int modme = 1e9 + 7;
 
     int zigZagArrays(int n, int l, int r) {
-        int m = r - l + 1;
 
-        vector<array<int,3>> prev(m), curr(m);
+        int start = 1;
+        int end = r - l + 1;
 
-        for(int i = 0; i < m; i++) {
-            prev[i][0] = 1;
-            prev[i][1] = 1;
-            prev[i][2] = 1;
-        }
+        vector<vector<int>> next(end + 1, vector<int>(2, 1));
+        vector<vector<int>> curr(end + 1, vector<int>(2, 0));
 
-        for(int len = 1; len < n; len++) {
+        int retme = 0;
 
-            vector<long long> pref0(m + 1, 0);
-            vector<long long> pref1(m + 1, 0);
+        vector<int> fortrue(end + 1, 0);
+        vector<int> forfalse(end + 1, 0);
 
-            for(int i = 0; i < m; i++) {
-                pref0[i + 1] = (pref0[i] + prev[i][0]) % MOD;
-                pref1[i + 1] = (pref1[i] + prev[i][1]) % MOD;
+        for(int index = n - 1; index >= 1; index--) {
+
+            fill(fortrue.begin(), fortrue.end(), 0);
+            fill(forfalse.begin(), forfalse.end(), 0);
+
+            for(int i = start; i <= end; i++) {
+                fortrue[i] =
+                    (fortrue[i - 1] + next[i][false]) % modme;
+
+                forfalse[i] =
+                    (forfalse[i - 1] + next[i][true]) % modme;
             }
 
-            for(int prevPos = 0; prevPos < m; prevPos++) {
+            for(int prev = start; prev <= end; prev++) {
 
-                long long greaterSum =
-                    (pref0[m] - pref0[prevPos + 1] + MOD) % MOD;
+                curr[prev][true] =
+                    (fortrue[end] - fortrue[prev] + modme) % modme;
 
-                long long smallerSum =
-                    pref1[prevPos];
-
-                curr[prevPos][0] = smallerSum;
-                curr[prevPos][1] = greaterSum;
-                curr[prevPos][2] = (greaterSum + smallerSum) % MOD;
+                curr[prev][false] =
+                    forfalse[prev - 1];
             }
 
-            swap(prev, curr);
+            swap(curr, next);
         }
 
-        long long ans = 0;
+        for(int i = start; i <= end; i++) {
 
-        for(int i = 0; i < m; i++) {
-            ans = (ans + prev[i][2]) % MOD;
+            retme = (retme + next[i][true]) % modme;
+            retme = (retme + next[i][false]) % modme;
         }
 
-        return (int)ans;
+        return retme;
     }
 };
