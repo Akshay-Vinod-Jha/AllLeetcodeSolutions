832e60dd2b4f67b04dc3b2da7b62da9091af7808
Jha Akshay Vinod <141563755+Akshay-Vinod-Jha@users.noreply.github.com>
2026-08-06 01:58:34 +0530

    Time: 722 ms (46.16%), Space: 0B (100%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,43 +1,7 @@
-
-select
-lt.user_id,
-IFNULL(round(lt.confirmed_count/(lt.confirmed_count+rt.timeout_count),2),0) as confirmation_rate 
-from 
-(
-    select 
-    lt.user_id,
-    IFNULL(confirmed_count,0) as confirmed_count
-    from 
-    Signups as lt
-    left join
-    (
-        select 
-    user_id,
-    count(*) as confirmed_count
-    from 
-    Confirmations
-    where action = 'confirmed'
-group by user_id
-    )  as rt
-    on lt.user_id = rt.user_id
-) as lt
-join (
-    select 
-    lt.user_id,
-    IFNULL(timeout_count,0) as timeout_count
-    from 
-    Signups as lt
-    left join
-    (
-        select 
-    user_id,
-    count(*) as timeout_count
-    from 
-    Confirmations
-    where action = 'timeout'
-group by user_id
-    )  as rt
-    on lt.user_id = rt.user_id
-) as rt
-
-on lt.user_id = rt.user_id
+SELECT 
+    s.user_id, 
+    ROUND(IFNULL(AVG(c.action = 'confirmed'), 0), 2) AS confirmation_rate
+FROM Signups AS s
+LEFT JOIN Confirmations AS c 
+    ON s.user_id = c.user_id
+GROUP BY s.user_id;
