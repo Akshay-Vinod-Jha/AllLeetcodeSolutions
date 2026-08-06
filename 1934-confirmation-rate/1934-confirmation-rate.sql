43fdbb0fe6f12f7bfcdd6d52113dccf507b3c3fb
Jha Akshay Vinod <141563755+Akshay-Vinod-Jha@users.noreply.github.com>
2026-08-06 15:13:29 +0530

    Time: 796 ms (25.63%), Space: 0B (100%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,7 +1,6 @@
 SELECT 
     s.user_id, 
     ROUND(IFNULL(AVG(c.action = 'confirmed'), 0), 2) AS confirmation_rate
-FROM Signups AS s
-LEFT JOIN Confirmations AS c 
-    ON s.user_id = c.user_id
+FROM Signups s
+LEFT JOIN Confirmations c ON s.user_id = c.user_id
 GROUP BY s.user_id;
